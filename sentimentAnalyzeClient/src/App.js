f50a9e5668f65d154c1797d2b84f9564540519e5
pavloteyfel <pavloteyfel@gmail.com>
2021-12-25 00:03:58 +0100

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -10,11 +10,12 @@ class App extends React.Component {
   value of the state, will be returned. The initial input mode
   is set to text
   */
-  state = {innercomp:<textarea rows="4" cols="50" id="textinput"/>,
-            mode: "text",
-          sentimentOutput:[],
-          sentiment:true
-        }
+  state = {
+    innercomp:<textarea rows="4" cols="50" id="textinput"/>,
+    mode: "text",
+    sentimentOutput:[],
+    sentiment:true
+  }
   
   /*
   This method returns the component based on what the input mode is.
@@ -49,9 +50,9 @@ class App extends React.Component {
         let output = data.label;
         let color = "white"
         switch(output) {
-          case "positive": color = "black";break;
-          case "negative": color = "black";break;
-          default: color = "black";
+          case "positive": color = "green"; break;
+          case "negative": color = "red"; break;
+          default: color = "yellow";
         }
         output = <div style={{color:color,fontSize:20}}>{output}</div>
         this.setState({sentimentOutput:output});
@@ -71,6 +72,9 @@ class App extends React.Component {
   })})  ;
   }
   
+  componentDidMount = () => {
+    document.title = "Sentiment Analyzer";
+  };
 
   render() {
     return (  
