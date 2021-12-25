f9ed31890a4878685cf5157c74ac347eb996b918
pavloteyfel <pavloteyfel@gmail.com>
2021-12-25 15:38:07 +0100

    feature: Add new GUI features

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import './bootstrap.min.css';
+import React from "react";
+import "./bootstrap.min.css";
+import './EmotionTable.css';
 
 class EmotionTable extends React.Component {
-    render() {
-      //Returns the emotions as an HTML table
-      return (  
-        <div>
-          <table className="table table-bordered">
-            <tbody>
-            {
-              /*Write code to use the .map method that you worked on in the 
-              Hands-on React lab to extract the emotions. If you are stuck,
-              please click the instructions to see how to implement a map*/
-            }
-            </tbody>
-          </table>
-          </div>
-          );
-        }
-    
+  render() {
+    return (
+      <div className="emotion-table">
+        <br />
+        <table className="table table-bordered table-sm">
+          <tbody>
+            {Object.entries(this.props.emotions).map(function (mapentry) {
+              return (
+                <tr>
+                  <td>{mapentry[0]}</td><td>{`${(mapentry[1] * 100).toFixed(2)}%`}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 export default EmotionTable;
